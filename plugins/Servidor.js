fc916d03d9edd8c3b803a99401bfb7038a6c420e
Ivan <162828351+elias1524@users.noreply.github.com>
2024-08-03 18:03:22 -0400

    Actualizar Servidor.js

--- plugins/Servidor.js
+++ plugins/Servidor.js
@@ -1,15 +1,15 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
 let txt = `PARA LOS QUE QUIERAN PATROCINARNOS EN SUS PROYECTOS
 
-*✨ HOLA BIENVENIDO/A TE PRESENTO ~CODESANDBOX~*
+*✨ HOLA BIENVENIDO/A TE PRESENTO OLYMPUS-HOST*
 
-*¿QUE ES CODESANDBOX?*
+*¿QUE ES OLYMPUS-HOST*
 _PLATAFORMA DONDE PUEDES ALOJAR MÚLTIPLES PROYECTOS A LARGO ALCANZE CON MUCHOS IDIOMAS Y CUSTOMIZADO_
 
 *¿QUE VENTAJAS TIENE Y QUE DIFERENCIA DE OTROS HOTS?*
 _LA DIFERENCIA ES QUE LA MAYORIA DE HOST MANTIENEN SOLO LOS LENGUAJES DE PROGRAMACION HABITUALES COMO "JAVASCRIPT" - "HTML" - "PYTHON" ADEMAS DE QUE ESTOS PUEDEN COSTAR UN CIERTO COSTO Y/O ALCANCE ADEMAS DE QUE NO PUEDES ALOJAR TANTOS DEBIDO A ALGUNA LIMITACIÓN O SU DEBIDO DINERO._
 
-_CODESANDBOX ES UN LUGAR GRATUITO DONDE TAMBIÉN ADEMAS DE QUE TENGAS UNA CUENTA, PUEDES CONECTAR TUS PROYECTOS PRIVADOS/PUBLICOS DE GITHUB, AMPLIO SISTEMA DE SOPORTE Y CUSTOMIZACION_
+_OLYMPUS-HOST ES UN LUGAR GRATUITO DONDE TAMBIÉN ADEMAS DE QUE TENGAS UNA CUENTA, PUEDES CONECTAR TUS PROYECTOS PRIVADOS/PUBLICOS DE GITHUB, AMPLIO SISTEMA DE SOPORTE Y CUSTOMIZACION_
 
 _🌟 CONTAMOS CON SOPORTE GARANTIZADO Y EXTRA_
 
@@ -21,46 +21,42 @@ _SI, CONTAMOS CON UNA VERSION "VIP" O "PRO" EL CUAL PERMITE AL USUARIO  USAR LEN
 *¿COMO PUEDO INICIAR?*
 _INICIAS O ALOJAS DE FORMA GRATUITA CON 4GB DE ESPACIO PARA CUALQUIER ESPECIALIDAD Y SI QUIERES AUMENTARLO ES PAGANDO DE FORMA VIP O EN SORTEOS OFICIALES_
 
-*_PAGINA_*
-_https://codesandbox.io/dashboard_
-
 *_CANAL DE WHATSAPP_*
-_https://whatsapp.com/channel/0029VagAcYlGE56u6WANKc2w_
+_https://whatsapp.com/channel/0029ValCkNT2ER6gHWFRQ71J_
 
 *_CREADOR_*
-_wa.me/5493795319022_
+_wa.me/593979133620_
 
 *_PAGOS PARA EL VIP_*
-_wa.me/5493795297363_
-*\`🍁 Te presentamos un nuevo server: "codesandbox"\`*
+_wa.me/593979133620_
+*\`🍁 Te presentamos un nuevo server: "OLYMPUS-HOST"\`*
 
 *¿Muy lento tu Samsung J1 y necesitas tener activo tu bot 24/7?*
 
 > *Te tenemos la mejor opción para que tengas activo tu bot 24/7, a precios muy accesibles. Es muy barato y todos pueden comprar.*
 
-Contamos con:
+*PRECIOS*
 
-Plan: Free
-Para: Todos usuarios
+• 1GB, 100CPU = 1 dólar
 
-Plan: Pro
-Para: Usuarios que paguen
-Precio: 12$ USD
+• 2GB, 120CPU = 2,50 dólares
 
-Plan: Enterprise - Empresas
-Para: Empresas
-Precio: Solo acordado y dado por el creador nada mas
+• 3GB, 140CPU = 3,50 dólares
+
+• 4GB, 175CPU = 4,50 dólares
+
+• 5GB, 200CPU = 5,50 dólares
 
 🔵 \`\`\`Información del Server\`\`\`
 
 💻 *Página:*
-https://codesandbox.io/dashboard
+
 
 💙 *Canal de WhatsApp:*
-https://whatsapp.com/channel/0029VaanL8K4yltYHri0ea3K
+https://whatsapp.com/channel/0029ValCkNT2ER6gHWFRQ71J
 
 📱 *Contacto*
-wa.me/5493794297363`
+wa.me/593979133620`
 await conn.sendMessage(m.chat, { text: txt,
 contextInfo:{
 forwardingScore: 9999999,
@@ -68,11 +64,11 @@ isForwarded: false,
 "externalAdReply": {
 "showAdAttribution": true,
 "containsAutoReply": true,
-title: `🍁 CODESANDBOX 🍁`,
+title: `✨ OLYMPUS-HOST ✨`,
 body: `✔️ Servidor de Calidad`,
 "previewType": "PHOTO",
-thumbnailUrl: 'https://telegra.ph/file/37478c04c4abeb4d02f80.jpg', 
-sourceUrl: 'https://whatsapp.com/channel/0029VaanL8K4yltYHri0ea3K'}}},
+thumbnailUrl: '', 
+sourceUrl: 'https://whatsapp.com/channel/0029ValCkNT2ER6gHWFRQ71J'}}},
 { quoted: fkontak})
 }
 handler.tags =['info'] 
