2baa565fc173ffef7fa4bdf1e850db30dd0c0837
Ivan <162828351+elias1524@users.noreply.github.com>
2024-08-06 18:18:32 -0400

    Actualizar Prueba200.js

--- plugins/Prueba200.js
+++ plugins/Prueba200.js
@@ -4,10 +4,23 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 let img = await (await fetch(`https://telegra.ph/file/53dc136ca4fdf6d852d8b.jpg`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
-let txt = *`BERMUDA`*
-await conn.sendFile(m.chat, img, "Thumbnail.jpg", txt, m, null, rcanal)
+let handler = async (m, { conn, command, usedPrefix }) => {
+let txt = `*BERMUDA*`
+await conn.sendMessage(m.chat, { text: txt,
+contextInfo:{
+forwardingScore: 9999999,
+isForwarded: false, 
+"externalAdReply": {
+"showAdAttribution": true,
+"containsAutoReply": true,
+title: `BERMUDA`,
+body: `BERMUDA`,
+"previewType": "PHOTO",
+thumbnailUrl: 'https://telegra.ph/file/53dc136ca4fdf6d852d8b.jpg', 
+sourceUrl: ''}}},
+{ quoted: fkontak})
 }
-handler.help = ['Cd']
-handler.tags = ['vd']
-handler.command = /^(Md)$/i
+handler.tags =['Bermuda'] 
+handler.help = ['Bermuda', 'Bermuda', 'Bermuda'] 
+handler.command = /^(Bermuda|Bermuda|Bermuda|Bermuda)$/i
 export default handler
