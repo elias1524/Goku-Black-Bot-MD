03a5f07d9104e7e128c14868cfdf55e3a74a8bdf
Ivan <162828351+elias1524@users.noreply.github.com>
2024-08-06 18:52:11 -0400

    Actualizar Prueba200.js

--- plugins/Prueba200.js
+++ plugins/Prueba200.js
@@ -4,23 +4,10 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 let img = await (await fetch(`https://telegra.ph/file/53dc136ca4fdf6d852d8b.jpg`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
-let handler = async (m, { conn, command, usedPrefix }) => {
 let txt = `*BERMUDA*`
-await conn.sendMessage(m.chat, { text: txt,
-contextInfo:{
-forwardingScore: 9999999,
-isForwarded: false, 
-"externalAdReply": {
-"showAdAttribution": true,
-"containsAutoReply": true,
-title: `BERMUDA`,
-body: `BERMUDA`,
-"previewType": "PHOTO",
-thumbnailUrl: 'https://telegra.ph/file/53dc136ca4fdf6d852d8b.jpg', 
-sourceUrl: ''}}},
-{ quoted: fkontak})
+await conn.sendFile(m.chat, img, "Thumbnail.jpg", txt, m, null, rcanal)
 }
-handler.tags =['Bermuda'] 
-handler.help = ['Bermuda', 'Bermuda', 'Bermuda'] 
-handler.command = /^(Bermuda|Bermuda|Bermuda|Bermuda)$/i
+handler.help = ['Bermuda']
+handler.tags = ['Maps']
+handler.command = /^(Bermuda)$/i
 export default handler
