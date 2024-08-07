befc48f005f1bc8015659b7fbc896ee9e7ca191c
Ivan <162828351+elias1524@users.noreply.github.com>
2024-08-06 22:12:54 -0400

    Actualizar info-cuentas.js

--- plugins/info-cuentas.js
+++ plugins/info-cuentas.js
@@ -1,7 +1,7 @@
 import fs from "fs"
 let handler = async (m, { conn, command }) => {
 let estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {orderMessage: { itemCount : 2023, status: 1, surface : 1, message: 'By Ivan ©', orderTitle: 'Bang', thumbnail: fs.readFileSync('./storage/logos/Menu2.jpg'), sellerJid: '0@s.whatsapp.net'}}}
-let foto = './storage/logos/Menu2.jpg'
+let foto = './storage/logos/Ins.jpg'
 let texto = `*Sígueme en ig Cuenta oficial* *Que tenga un Buen dia*
 ${ivan}`
 await conn.sendFile(m.chat, foto, 'Curiosity.jpg', texto, estilo)}
